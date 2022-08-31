70b702316de5b11a35e95711a43e3467649c0b21
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-01 00:40:26 +0100

    chore: Added the functionalities like setinterval

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -3,6 +3,13 @@ import React, { useState } from "react";
 const Clock = () => {
   let time = new Date().toLocaleTimeString();
   const [currentTime, setCurrentTime] = useState(time);
+
+  const updateTime = () => {
+    let time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  };
+
+  setInterval(updateTime, 1000);
   return (
     <div className="page__container">
       <div>
