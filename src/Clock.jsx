947a5020ea67c97859c3fe93bdaf14e8056b05b6
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-01 00:32:24 +0100

    chore: Getting the local time

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Clock = () => {
-  return (
-    <div>
-      <h1>Digital Clock</h1>
+    return (
+      
+        let time = new Date().toLocaleTimeString();
+    <div className="page__container">
+      <div>
+        <h1>Digital Clock</h1>
+            <h2>{ time}</h2>
+      </div>
     </div>
   );
 };
